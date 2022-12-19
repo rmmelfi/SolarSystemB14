3b66390ee28af577891342fb90f12c2d5ccfd5fd
rmmelfi <rmmelfi@gmail.com>
2022-12-19 14:07:16 -0500

    Version1

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("The basic Structure is ready please writ down your code in this!"+ "project");
+    }
+}
